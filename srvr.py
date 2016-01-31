5ab06cd343fb531819273975f81b3897b4b20c6a
misterdanb <danb@hasi.it>
2016-01-31 10:27:32 +0100

    Add more configuration options

--- srvr.py
+++ srvr.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 import uinput
 import time
-import select
+import json
 import threading
 import socket
 import time
@@ -12,6 +12,12 @@ import argparse
 
 l = None
 
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    length = len(value)
+
+    return tuple(int(value[i:i + length // 3], 16) for i in range(0, length, length // 3))
+
 class C0ntroller(uinput.Device):
     JOY_UP_MASK = 0x01
     JOY_RIGHT_MASK = 0x02
@@ -21,8 +27,6 @@ class C0ntroller(uinput.Device):
     BTN_A_MASK = 0x20
     BTN_B_MASK = 0x40
     BTN_Y_MASK = 0x80
-
-    KEEP_ALIVE_TIMEOUT = 10
 
     EVENTS = (
         uinput.BTN_0,
@@ -46,31 +50,25 @@ class C0ntroller(uinput.Device):
     def refresh(self):
         self.refresh_time = time.time()
 
-    def is_alive(self):
-        return time.time() - self.refresh_time < self.KEEP_ALIVE_TIMEOUT
+    def dead_time(self):
+        return time.time() - self.refresh_time
 
     def fire(self, event):
         event_diff = event ^ self.prev_event
 
         if event_diff != 0:
-            if event_diff & self.JOY_UP_MASK != 0:
-                if event & self.JOY_UP_MASK != 0:
+            if event_diff & (self.JOY_UP_MASK | self.JOY_DOWN_MASK) != 0:
+                if event & self.JOY_UP_MASK != 0 and not event & self.JOY_DOWN_MASK != 0:
+                    self.emit(uinput.ABS_Y, 0)
+                elif event & self.JOY_DOWN_MASK != 0 and not event & self.JOY_UP_MASK != 0:
                     self.emit(uinput.ABS_Y, 255)
                 else:
                     self.emit(uinput.ABS_Y, 128)
-            elif event_diff & self.JOY_DOWN_MASK != 0:
-                if event & self.JOY_DOWN_MASK != 0:
-                    self.emit(uinput.ABS_Y, 0)
-                else:
-                    self.emit(uinput.ABS_Y, 128)
 
-            if event_diff & self.JOY_LEFT_MASK != 0:
-                if event & self.JOY_LEFT_MASK != 0:
+            if event_diff & (self.JOY_LEFT_MASK | self.JOY_RIGHT_MASK) != 0:
+                if event & self.JOY_LEFT_MASK != 0 and not event & self.JOY_RIGHT_MASK != 0:
                     self.emit(uinput.ABS_X, 0)
-                else:
-                    self.emit(uinput.ABS_X, 128)
-            elif event_diff & self.JOY_RIGHT_MASK != 0:
-                if event & self.JOY_RIGHT_MASK != 0:
+                elif event & self.JOY_RIGHT_MASK != 0 and not event & self.JOY_LEFT_MASK != 0:
                     self.emit(uinput.ABS_X, 255)
                 else:
                     self.emit(uinput.ABS_X, 128)
@@ -117,13 +115,33 @@ class S3rver(object):
     CMD_PROPAGATE_HOST = 0x30
     CMD_ASK_HOST = 0x31
 
-    def __init__(self, port):
+    def __init__(self, port, config=None):
         self.port = port
         self.controllers = {}
 
+        no_config_present = False
+
+        if config:
+            try:
+                with open(config) as config_file:
+                    config_data = json.load(config_file)
+
+                    l.info("Loaded config file {0}...".format(config))
+
+                    self.color = hex_to_rgb(config_data["color"])
+                    self.timeout = config_data["timeout"]
+            except:
+                no_config_present = True
+        else:
+            no_config_present = True
+
+        if no_config_present:
+            self.color = (0xFF, 0xFF, 0xFF)
+            self.timeout = 10
+
     def propagate_host(self):
         l.debug("Propagating the new game server (me)...")
-        set_host_msg = "\x30\x00\x00\xFF\x0F\x30"
+        set_host_msg = bytes([ 0x30, self.color[0], self.color[1], self.color[2], 0x0F, 0x30 ])
         self.sock.sendto(set_host_msg, ("<broadcast>", self.port))
 
     def serve(self):
@@ -131,6 +149,7 @@ class S3rver(object):
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
+        self.sock.settimeout(self.timeout)
 
         self.server_addr = (self.HOST, self.port)
         self.sock.bind(self.server_addr)
@@ -138,34 +157,48 @@ class S3rver(object):
         self.propagate_host()
 
         while True:
-            data, client_addr = self.sock.recvfrom(self.MSG_SIZE)
+            try:
+                data, client_addr = self.sock.recvfrom(self.MSG_SIZE)
 
-            l.debug("Received data from {0}:".format(client_addr[0]) + str([ int(ord(c)) for c in data ]))
+                l.debug("Received data from {0}: ".format(client_addr[0]) + str(data))
 
-            if len(data) != self.MSG_SIZE:
-                l.warn("{0}: Not a valid command: wrong packet length!".format(client_addr))
-                continue
+                if len(data) != self.MSG_SIZE:
+                    l.warn("{0}: Not a valid command: wrong packet length!".format(client_addr))
+                    continue
 
-            if data[0] != data[self.MSG_SIZE - 1]:
-                l.warn("{0}: Not a valid command: first byte and last byte mismatching!".format(client_addr))
-                continue
+                if data[0] != data[self.MSG_SIZE - 1]:
+                    l.warn("{0}: Not a valid command: first byte and last byte mismatching!".format(client_addr))
+                    continue
 
-            if ord(data[0]) == self.CMD_PROPAGATE_HOST:
-                continue
+                if data[0] == self.CMD_PROPAGATE_HOST:
+                    continue
 
-            if not client_addr in self.controllers:
-                self.controllers[client_addr] = C0ntroller(client_addr[0])
-                l.info("New client {0} connected!".format(client_addr[0]))
+                if data[0] in [ self.CMD_KEEP_ALIVE, self.CMD_BUTTONS_CHANGED, self.CMD_ASK_HOST ]:
+                    if client_addr in self.controllers:
+                        self.controllers[client_addr].refresh()
+                    else:
+                        l.info("New client {0} connected!".format(client_addr[0]))
+                        self.controllers[client_addr] = C0ntroller(client_addr[0])
 
-            if ord(data[0]) == self.CMD_KEEP_ALIVE:
-                self.controllers[client_addr].fire(ord(data[1]))
-                self.controllers[client_addr].refresh()
-            elif ord(data[0]) == self.CMD_BUTTONS_CHANGED:
-                self.controllers[client_addr].fire(ord(data[1]))
-                self.controllers[client_addr].refresh()
-            elif ord(data[0]) == self.CMD_ASK_HOST:
-                l.debug("Client asked for a game server.")
-                self.propagate_host()
+                if data[0] == self.CMD_KEEP_ALIVE:
+                    self.controllers[client_addr].fire(data[1])
+                elif data[0] == self.CMD_BUTTONS_CHANGED:
+                    self.controllers[client_addr].fire(data[1])
+                elif data[0] == self.CMD_ASK_HOST:
+                    l.debug("Client asked for a game server.")
+                    self.propagate_host()
+            except socket.timeout:
+                pass
+
+            clients_to_kick = []
+
+            for client_addr in self.controllers:
+                if self.controllers[client_addr].dead_time() > self.timeout:
+                    clients_to_kick.append(client_addr)
+
+            for client_addr in clients_to_kick:
+                l.info("Kicked client {0}! (timeout)".format(client_addr[0]))
+                del self.controllers[client_addr]
 
 def main():
     global l
@@ -173,6 +206,7 @@ def main():
     p = argparse.ArgumentParser()
     p.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
     p.add_argument("-p", "--port", type=int, default=1337, help="use another port")
+    p.add_argument("-c", "--config", type=str, default="srvr.cnfg", help="use another config file")
 
     args = p.parse_args()
 
@@ -190,12 +224,12 @@ def main():
         l.setLevel(logging.INFO)
 
     l.info("Loading uinput kernel module, if it has not been loaded yet...")
-    dev_null = open(os.devnull, 'w')
+    dev_null = open(os.devnull, "w")
     result = subprocess.call([ "modprobe", "uinput"], stdout=dev_null, stderr=dev_null)
     dev_null.close()
 
     if result == 0:
-        server = S3rver(args.port)
+        server = S3rver(args.port, args.config)
         server.serve()
     else:
         l.error("Could not load uinput kernel module! Am I root?")
